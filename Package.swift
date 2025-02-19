112e97e2f3e839719ecaf28a8a57b25f18e4930a
Ashish Gupta <ashish.gupta@sign3labs.com>
2025-02-19 13:47:04 +0530

    increase version to 0.0.1007

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "sign3intelligence-ios-sdk-swift-package",
-            url: "https://firebasestorage.googleapis.com/v0/b/make-friends-7338f.appspot.com/o/test-intelligence-iOS%2F0.0.1007%2FSign3Intelligence.xcframework.zip?alt=media&token=783e74d2-c078-4232-890a-162c309eee90",
-            checksum: "5564b6f5412dd0ab6cb3eb825387afcab34b3619db454cc9cab81c93573fb02f"
+            url: "https://firebasestorage.googleapis.com/v0/b/make-friends-7338f.appspot.com/o/test-intelligence-iOS%2F0.0.1007%2FSign3Intelligence.xcframework.zip?alt=media&token=93743c84-a9a9-412f-9a40-bf6627a64152",
+            checksum: "a860aa9ece7e71e3e0c8f967999d07b23cef0cf8546a00ebccc39cb2b31afc86"
         )
     ]
 )
