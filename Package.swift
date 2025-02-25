7121ed226c4bc41137b79238d91ac5ce40e2cafa
sreyans004 <sreyans@sign3labs.com>
2025-02-25 18:52:33 +0530

    Upgraded package version to 0.0.1010

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-
+let version = "0.0.1010"
 let package = Package(
     name: "sign3intelligence-ios-sdk-swift-package",
     products: [
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "sign3intelligence-ios-sdk-swift-package",
-            url: "https://firebasestorage.googleapis.com/v0/b/make-friends-7338f.appspot.com/o/test-intelligence-iOS%2F0.0.1009%2FSign3Intelligence.xcframework.zip?alt=media&token=48222ada-77f0-4b0b-8742-e6876f0aad02",
+            url: "https://cdn.sign3.in/mobile-sdk/ios/v\(version).zip",
             checksum: "f63181c4af20a2a07ad05987ba2d18c7142bfcd33b34af5f06074baf628e99bb"
         )
     ]
