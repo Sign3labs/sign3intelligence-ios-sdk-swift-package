b3b5b711ffe8fc49086b83398e623fc2c9876e31
Ashish Gupta <ashish.gupta@sign3labs.com>
2025-04-18 18:42:55 +0530

    Change package name sign3intelligence-ios-sdk-swift-package to  Sign3Intelligence.

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 let version = "0.0.1018"
 let package = Package(
-    name: "sign3intelligence-ios-sdk-swift-package",
+    name: "Sign3Intelligence",
     products: [
         .library(
-            name: "sign3intelligence-ios-sdk-swift-package",
-            targets: ["sign3intelligence-ios-sdk-swift-package"]),
+            name: "Sign3Intelligence",
+            targets: ["Sign3Intelligence"]),
     ],
     targets: [
         .binaryTarget(
-            name: "sign3intelligence-ios-sdk-swift-package",
+            name: "Sign3Intelligence",
             url: "https://cdn.sign3.in/mobile-sdk/ios/test/v\(version)/Sign3Intelligence.xcframework.zip",
             checksum: "b78ba807707723c5201ecd1f61cae055bd4020c78a9c8bd314070517b4b5b071"
         )
