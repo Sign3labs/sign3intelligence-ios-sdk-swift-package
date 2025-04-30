fb36e8282a91e4bbb502303500f22183f796b1f3
Ashish Gupta <ashish.gupta@sign3labs.com>
2025-04-30 15:12:11 +0530

    version bump to 1.0.4

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let version = "1.0.3"
+let version = "1.0.4"
 let package = Package(
     name: "Sign3Intelligence",
     products: [
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Sign3Intelligence",
-            url: " https://cdn.sign3.in/mobile-sdk/ios/intelligence/v\(version)/Sign3Intelligence.xcframework.zip",
-            checksum: "11711b5199d96d9f59c85d472ee8c843d3a61a3a8a057b69c4041fe4bacb81d6"
+            url: "https://cdn.sign3.in/mobile-sdk/ios/intelligence/v\(version)/Sign3Intelligence.xcframework.zip",
+            checksum: "e4a4aa67bf064a3d75332d419bd7c50b4f48526e0f7a141465db0eab33c83001"
         )
     ]
 )
