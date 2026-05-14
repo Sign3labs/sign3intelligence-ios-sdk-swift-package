66eeea0299d899d4eb9eb491fcadc7f73c38f2ee
Ashish Gupta <ashish.gupta@sign3labs.com>
2026-05-14 17:31:00 +0530

    version 1.1.3

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let version = "0.0.6265"
+let version = "1.1.3"
 let package = Package(
     name: "Sign3Intelligence",
     products: [
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "Sign3Intelligence",
             url: "https://cdn.sign3.in/mobile-sdk/ios/intelligence/v\(version)/Sign3Intelligence.xcframework.zip",
-            checksum: "68b7ab2e208c8967339827a030f4a915c4aa848867081767acf110cff49dca51"
+            checksum: "9b9c54beda952300deb247fa593a9f7f52b5f6df5ae5eff1ff24df7e08dd0388"
         )
     ]
 )
