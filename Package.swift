88b6e7b2cfee5f444643a0dc1c9cf3cc43f01813
Ashish Gupta <ashish.gupta@sign3labs.com>
2026-05-19 18:38:52 +0530

    Version bump to 1.1.5 and checksum update

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let version = "1.1.4"
+let version = "1.1.5"
 let package = Package(
     name: "Sign3Intelligence",
     products: [
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "Sign3Intelligence",
             url: "https://cdn.sign3.in/mobile-sdk/ios/intelligence/v\(version)/Sign3Intelligence.xcframework.zip",
-            checksum: "d1039b8a9565a138952d0bcf180c553b671677637b13829b46ce3fe48dacb573"
+            checksum: "699ace8ff657fb7c8a89655f368d2dad3c0f4bdb5f3e2fff3e7d75375fe8fe60"
         )
     ]
 )
