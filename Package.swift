219c22197a085a064366614e8e484c9d937fdbc2
Ashish Gupta <ashish.gupta@sign3labs.com>
2026-09-04 11:18:47 +0530

    increase version to 1.1.8 and update checksum.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let version = "2.0.0"
+let version = "1.1.8"
 let package = Package(
     name: "Sign3Intelligence",
     products: [
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Sign3Intelligence",
-            url: "https://cdn.sign3.in/mobile-sdk/ios/intelligence/v\(version)/Sign3Intelligence.xcframework.zip",
-            checksum: "82f252f5a517712975f6a865e427fb912feacebc28286f5ef0a74d4007805d68"
+            url: "https://cdn.sign3.in/mobile-sdk/ios/intelligence/v.\(version)/Sign3Intelligence.xcframework.zip",
+            checksum: "ee51678e445be2b813c90684d4b5a98399f7e7d5edac057495a4e0d2c3ac0d92"
         )
     ]
 )
