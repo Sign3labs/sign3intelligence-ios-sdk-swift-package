1c5f079bab3c70e2e49c8435d00a169e72f93591
Ashish Gupta <ashish.gupta@sign3labs.com>
2026-09-25 12:29:14 +0530

    add simbinding version in test branch

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let version = "0.0.1000"
 let package = Package(
     name: "Sign3Intelligence",
     products: [
@@ -13,8 +12,10 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Sign3Intelligence",
-            url: "https://cdn.sign3.in/mobile-sdk/ios/test/intelligence/v\(version)/Sign3Intelligence.xcframework.zip",
-            checksum: "1e779d2af62abc03341873c569620fb42619ca763e8129a3ab2ebad79fb7cd0a"
+//            url: "https://cdn.sign3.in/mobile-sdk/ios/test/intelligence/v0.0.1000/Sign3Intelligence.xcframework.zip",
+//            checksum: "1e779d2af62abc03341873c569620fb42619ca763e8129a3ab2ebad79fb7cd0a"
+            url: "https://cdn.sign3.in/mobile-sdk/ios/test/intelligence/v0.0.2000/Sign3Intelligence.xcframework.zip",
+            checksum: "bafabc25cb8b464a9fdf3729051fe6252a450bfa52caa0f02f9ef318740ad142"
         )
     ]
 )
